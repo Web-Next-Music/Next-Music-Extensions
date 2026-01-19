48d1efcb94eff3a1454cd96f3c6ae6b6e1f3be51
Diramix <danyagranny10@proton.me>
2026-01-19 23:11:29 +0300

    new addons & fixes

--- Fullscreen Quality Cover Fix/README.md
+++
@@ -1,2 +0,0 @@
-# Fullscreen Quality Cover Fix 1.0.0
-Исправление качества Cover картинки в фуллскрине.
---
+++ Loves-Green-Lemon-1.0.0/script.js
@@ -0,0 +1,27 @@
+// Скрипт для смены темы
+function yandexThemeUpdate() {
+  const body = document.body;
+  if (
+    !body.classList.contains("ym-light-theme") &&
+    !body.classList.contains("ym-dark-theme")
+  ) {
+    body.classList.add("ym-dark-theme");
+  } else if (body.classList.contains("ym-light-theme")) {
+    body.classList.replace("ym-light-theme", "ym-dark-theme");
+  }
+}
+
+yandexThemeUpdate();
+
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.attributeName === "class") {
+      yandexThemeUpdate();
+    }
+  }
+});
+
+observer.observe(document.body, {
+  attributes: true,
+  attributeFilter: ["class"],
+});
